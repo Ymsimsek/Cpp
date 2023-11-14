b3d9f3bc67e42943cb28eac529867d8264b4022b
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-15 01:55:18 +0300

    Update Const L Value References.cpp

--- Reference Semantics/Const L Value References.cpp
+++ Reference Semantics/Const L Value References.cpp
@@ -26,4 +26,37 @@ r=45; // Because r=x and r can not be equalized with another variable but x can 
 
 */ 
 
+// -> const L value references can be initialized with R value expressions. 
 
+const int& r{10};
+
+/*
+
+int temp_obj1{10};
+const int& rr{temp_obj1};
+
+*/
+
+//Similarly
+
+int ival{5};
+const double& dr{ival};
+
+/* 
+
+double temp_obj2{ival};
+const double& fr{temp_obj2};
+
+*/
+
+  
+/*
+
+          |   L Value    |    const L Value    |    R Value    |
+  ________|______________|_____________________|_______________|
+  T&      |  Can Bind    |    Can not Bind     |    Can Bind   |
+  ________|______________|_____________________|_______________|
+  const T&|  Can Bind    |    Can not Bind     |    Can Bind   |
+  ________|______________|_____________________|_______________|
+
+*/
