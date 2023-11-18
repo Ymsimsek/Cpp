cc35d28b7c507b32b51f4396db5118a5ddd441dc
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-18 21:49:01 +0300

    Update Introduction to Type Deduction & Auto Keyword.cpp

--- Type Deduction/Introduction to Type Deduction & Auto Keyword.cpp
+++ Type Deduction/Introduction to Type Deduction & Auto Keyword.cpp
@@ -46,3 +46,33 @@ void Example7(){
   auto x=func;//int(*)int: function names becomes function adresses.
 }
 
+// Geri dönüş türü referans olan fonksiyonların auto ile çağrılırken auto'nun aldığı değer referanstan bağımsızdır.
+
+// L ref auto:
+
+void Example8(){
+  const int x{10};
+  auto &y{x}; //auto -> const int&, referans deklaratörü olan ifadeler auto ile çağrıldığında const ifade düşmez. 
+}
+
+// Array auto:
+
+void Example10(){
+  int a[5]{};
+  auto &b{a};// int[5]
+}
+
+// Multi dimensional or one dimensional arrays can be declared using auto keyword.
+
+void Example11(){
+  auto &x {"eren"};// auto-> const char[5]
+}
+
+// Function ref:
+
+void Example12(){
+  int foo(int);
+  auto &f{foo};// auto-> int(int)
+  
+}
+
