29b7909a58cc5a9fd994b0140b786d7416b333f9
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-19 16:37:23 +0300

    Create "constexpr" keyword .cpp

---
+++ One Definition Rule (ODR)/"constexpr" keyword .cpp
@@ -0,0 +1,4 @@
+/* 
+const ifadesinden farklı olarak compile time'da asla değişmeyecek ifadelerle kullanılabilir. 
+Eğer tüm parametreler sabit ifadelerle çağrılırsa fonksiyonun geri dönüş değeri compile time'da bulunur
+*/
