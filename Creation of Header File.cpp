8fb1552f9519fe5931203bd4c8e45fcfd72a393c
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-22 20:12:09 +0300

    Create Creation of Header File.cpp

---
+++ Creation of Header File.cpp
@@ -0,0 +1,14 @@
+/* 
+C++'da header file oluştururken genel bilinenin yanında tanıma included kelimesi eklenirse ve daha önce bu header dosyası dahil edilmemişse, içerideki kodun çalışmasına izin verir.
+
+Ex:
+*/
+#ifndef NAMEOFHEADERFILE_INCLUDED
+#define NAMEOFHEADERFILE_INCLUDED
+
+//... Some code here 
+
+
+
+#endif
+
