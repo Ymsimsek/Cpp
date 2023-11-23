745b5886fdf42bfd66ca7200bf391e7f1ef64d6e
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-23 20:17:13 +0300

    Create Move Assignment.cpp

---
+++ Classes & OOP/Move Assignment.cpp
@@ -0,0 +1,11 @@
+/*
+Move Assignment Derleyici Tarafından Yazılışı:
+myClass&& operator=(myClass&& other){
+  //...
+  ax=std::move(other.ax);
+  bx=std::move(other.bx);
+  /..
+return *this;
+}
+
+*/
