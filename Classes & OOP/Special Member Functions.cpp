996dd0feb644feffc71b52fad83aa576ca8895bb
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-23 14:35:37 +0300

    Create Special Member Functions.cpp

---
+++ Classes & OOP/Special Member Functions.cpp
@@ -0,0 +1,17 @@
+/*
+Special Member Functions of Classes:
+- Defalut Ctor
+- Destructor
+- Copy Ctor
+- Move Ctor (C++11)
+- Copy Assignment
+- Move Assignment (C++11)
+
+-> Bu fonksiyonlara special member functions denmesinin sebebi bu fonksiyonların kodlarının (Belirli koşullar sağlandığında) derleyici tarafından yazılmasıdır. 
+
+-> Derleyicinin bu kod yazma işlemine default etmesi adı verilir. 
+
+-> C++'da Dtor, kullanılmayan nesnelerin ömrünü sonlandırmaya yarar.
+
+  */
+
