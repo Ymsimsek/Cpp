e515f6c782bb74f804cc8c8049b17b8297d906d9
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-23 14:15:53 +0300

    Create Good To Remember.cpp

---
+++ Classes & OOP/Good To Remember.cpp
@@ -0,0 +1,3 @@
+/*
+-> Eğer bir sınıfın veri elemanının değişmesi onun problem domainindeki algılanan değeriyle alakalı değilse const üye fonksiyonda çağırıp değiştirmemizde semantik açıdan bir hata değildir ancak syntax açısından 
+hatadır. Bu hatayı aşmamızı sağlayan araç "mutable" keyword'üdür. 
