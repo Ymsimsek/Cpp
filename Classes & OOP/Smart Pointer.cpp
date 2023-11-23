8cf9d5e1acce7a1dcb881eb97a41bf34430d697a
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-24 01:02:17 +0300

    Create Smart Pointer.cpp

---
+++ Classes & OOP/Smart Pointer.cpp
@@ -0,0 +1,10 @@
+/*
+-> Smart Pointer: Kendisi pointer olmayan, sınıf türünden bir nesnedir. Bazı araçlarla bir sınıf olmasına rağmen normal pointerler gibi deferencing ve ok operatörünün operandı olabilirler.
+Bunun yapılabilmesini operator overloading mekanizması sağlar. 
+
+-> Normal pointerların yapamadığı işlevleri vardır:
+-std::unique_ptr
+-std::shared_ptr
+
+*/
+  
