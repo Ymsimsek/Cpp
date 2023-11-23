576b71a56094ead1244da250c0d6cec085d0fd69
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-23 15:58:02 +0300

    Update "delete" Keyword.cpp

--- Classes & OOP/"delete" Keyword.cpp
+++ Classes & OOP/"delete" Keyword.cpp
@@ -1,1 +1,9 @@
+/*
+"delete" Bildirimi:
 
+-> Bir fonksiyonun delete bildirimi fonksiyonun var ancak yapılan çağrının geçersiz olduğunu söyler.
+
+-> Delete edilen function vardır ve function overload resolution'a dahil edilir. 
+
+*/
+  
