23130774b12ef9df82b7fcbfd08d47ccacf88c06
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-24 01:44:50 +0300

    Create Static Member Functions.cpp

---
+++ Classes & OOP/Static Member Functions.cpp
@@ -0,0 +1,5 @@
+/*
+Non-static member fonksiyonlarına çağrı yapmak için bir nesneye ihtiyaç vardır. Ancak static member fonksiyonları nesne tanımlanmadan da çağırılabilir.
+Tıpkı global fonksiyon gibidirler ancak class scope'ta yer alırlar. Name lookup'ta class scope'tan başlanır. Sınıfların static üye fonksiyonları yine sınıfın prive bölümüne erişebilirler. 
+
+*/
