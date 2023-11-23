e571e1ec97180f65eafee55ebc820b9f036ca398
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-23 15:23:53 +0300

    Update Good To Remember.cpp

--- Classes & OOP/Good To Remember.cpp
+++ Classes & OOP/Good To Remember.cpp
@@ -1,3 +1,7 @@
 /*
 -> Eğer bir sınıfın veri elemanının değişmesi onun problem domainindeki algılanan değeriyle alakalı değilse const üye fonksiyonda çağırıp değiştirmemizde semantik açıdan bir hata değildir ancak syntax açısından 
 hatadır. Bu hatayı aşmamızı sağlayan araç "mutable" keyword'üdür. 
+
+-> Eğer bir sınıf nesnesi için çağırıları ctor'un içinde, başka bir kaynak dosyadaki global bir sınıf değişkeni kullanıyorsak hayata gelmeden kullanma olasılığı meydana gelir ve tanımsız davranış olur. Buna static init fiasco denir. 
+  
+*/
