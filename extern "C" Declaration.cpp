16ef5065db875468d44d5403d019c59b51fe2913
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-25 00:21:07 +0300

    Create extern "C" Declaration.cpp

---
+++ extern "C" Declaration.cpp
@@ -0,0 +1,14 @@
+/* 
+extern "C"  
+
+-> extern "C" bildirimi, bir C kaynak dosyasında yazılmış ve bir header dosyasında bildirimi yapılmış ifadelerin C++ derleyicisine kodun C dilinde yazıldığını bildiren ifadedir.
+
+Ex:
+extern "C" int x;
+extern "C" double y;
+// veya 
+extern "C"{
+  int x;
+  double y;
+}
+*/
