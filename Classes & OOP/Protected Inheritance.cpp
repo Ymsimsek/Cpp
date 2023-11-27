5d423eba75148ccb9634ff9a5f6d7365dc5c6d18
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-27 18:38:54 +0300

    Create Protected Inheritance.cpp

---
+++ Classes & OOP/Protected Inheritance.cpp
@@ -0,0 +1,5 @@
+/*
+Genel özellikleriyle private inheritance'a benzer. Taban sınıfların public ve protected kısımları türetilen sınıfın protected bölümüne eklenir. Protected inheritance, multi-level inheritance devreye girdiğinde
+private inheritance'dan daha kullanışlı olur.
+
+*/
