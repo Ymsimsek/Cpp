fb554fccc9df0c67beeb39ec22d216e176d286c1
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-27 18:46:15 +0300

    Create Diamond Formation & Virtual Inheritance.cpp

---
+++ Classes & OOP/Diamond Formation & Virtual Inheritance.cpp
@@ -0,0 +1,8 @@
+/*
+Diamond Formation: Ortak taban sınıfa ait iki tğretilen sınıftan multiple inheritance yolu ile türetilen sınıfın içinde ortak taban sınıfından iki adet bulunur. Bu durumm compile ve run time'da
+önlem alınmazsa sorun oluşturabilir. Sınıf nesnesinin içindeki ortak taban nesnesinin sayısı virtual inheritance kullanılarak bire düşürülebilir.
+
+Virtual Inheritance: Eğer iki taban sınıfın ortak bir taban sınıfı varsa ve bunlar çoklu kalıtımda kullanılacaksa ve ortak alan sınıfı nesnesinden bir tane olması isteniyorsa her iki sınıfın da "virtual" keyword'ü 
+ile tanımlanması gereklidir. Bu duruma virtual inheritance denir. 
+
+*/
