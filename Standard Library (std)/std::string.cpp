b938dbb94cec1a313bf8f161d825b25ae3bd55a6
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-11-29 14:37:37 +0300

    Update std::string.cpp

--- Standard Library (std)/std::string.cpp
+++ Standard Library (std)/std::string.cpp
@@ -5,11 +5,11 @@ dinamik dizi sınıfıdır. Operator overloading'den çokça yararlanan bir küt
 -> String fonksiyonlarda, yazı uzunluğu türü, kapasite türü, bir ya da iki parmetre indeks değeri, karakterlerin adet sayısı, npos;"size_type" türü cinsindendir. (std::string::size_type)
 
 -> String fonksiyonlarına girilebilen bazı argümanlar ve temsiliyetleri:
-- const string& //whole string
+- const string& //whole string 
 - const string&, size_type idx//sub string, indexten string sonuna
 - const string&, size_type idx, size_type len //substring, indexten len kadar ileri
-- const char * //cstring
-- const char*, size_type len //Data parameter
+- const char* //cstring, son parametresi nullptr olan string
+- const char*, size_type len //Data parameter, char* adresinden len kadar ileri
 - char //karakter
 - size_type n ,char c //n adet c karakteri
 - iterator beg, iterator end //range parameter
