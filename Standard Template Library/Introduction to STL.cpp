39b60e8f495cb188f3ccbcd98be53c438b0169a7
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-12-15 17:53:10 +0300

    Create Introduction to STL.cpp

---
+++ Standard Template Library/Introduction to STL.cpp
@@ -0,0 +1,53 @@
+/*
+-> Dilin standart kütüphanesiyle aynı anlama gelmez. STL template'lerden oluşur ve template kütüphanesidir. 
+
+STL'in Temel Öğerleri:
+- Containers: Temel veri yapılarını implemente eden sınıf şablonlarıdır.
+- Iterators: Varlık sebebi containerlerde tutulan varlıkların konum bilgisinin tutulup kullanılmasını sağlayan sınıf şablonlarıdır. Her pointer bir iteratordür.
+Pointer-like bir arayüze sahiptirler. Operator overloading mekanizmaları kullanılır. Iterator algoritmaları ve containerları bir araya getirir.
+- Algorithms: Algoritmaları implemente eden generic fonksiyonlardır. Fonksiyon şablonlarıdır.
+
+Yardımcı Öğeler:
+- Container Adepters
+- Iterator Adepters
+- Function Adepters
+- Function Objects
+
+
+Container Categories:
+
+-Sequence Containers:
+std::vector
+std::deque
+std::list
+std::forward_list (C++11)
+std::array (C++11)
+std::string
+
+-Associative Containers:
+std::set
+std::multiset
+std::map
+std::multimap
+
+-Unordered Associative Containers:
+std::unordered_set (C++11)
+std::unordered_multiset (C++11)
+std::unordered_map (C++11)
+std::unordered_multimap (C++11)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
