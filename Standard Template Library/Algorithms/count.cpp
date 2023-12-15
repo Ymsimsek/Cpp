a22c8ef68fe9df2ce731bea86f0c0f5c3d2ec4e7
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-12-15 21:05:28 +0300

    Create count.cpp

---
+++ Standard Template Library/Algorithms/count.cpp
@@ -0,0 +1,14 @@
+/*
+#include <vector>
+#include <string>
+#include <algorithm>
+int main(){
+  using namespace std;
+
+  vector<string> svec;
+  rfill(svec,100'000,rname);
+  cout<<count(svec.begin(),svec.end(),"tamer")<<'\n';
+
+}
+
+*/
