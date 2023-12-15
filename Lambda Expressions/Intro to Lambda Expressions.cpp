1221adbf0197ac677a5d431af3afb44a87be2c4f
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-12-15 22:28:54 +0300

    Create Intro to Lambda Expressions.cpp

---
+++ Lambda Expressions/Intro to Lambda Expressions.cpp
@@ -0,0 +1,8 @@
+/*
+-> Derleyici lambda ifadelerini gördüğünde bir class definition yapar ve ifadenin kendisini de tanımladığı sınıf türünden bir geçici nesneye
+dönüştürür. Lambda expression kullanılan yerde aslında bir PR value türünden nesne, geçici nesne, kullanılmış olur.
+
+-> Derleyiciye sınıf kodu yazdırmanın bir yoludur. 
+
+
+*/
