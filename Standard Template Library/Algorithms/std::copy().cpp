0309c04458f36ae9352a1b54fc86b9c55d3424cc
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-12-15 19:29:25 +0300

    Update and rename Standard Template Library/Algorithms.cpp to Standard Template Library/Algorithms/std::copy().cpp

---
+++ Standard Template Library/Algorithms/std::copy().cpp
@@ -0,0 +1,8 @@
+/*
+-> Algoritmalar, exception throw etmezler.!!
+  
+template<typename InIter, typename OutIter>
+OutIter Copy(InIter beg, InIter end, OutIter destbeg){ //Önce kaynak sonra hedef.
+  
+    
+}
