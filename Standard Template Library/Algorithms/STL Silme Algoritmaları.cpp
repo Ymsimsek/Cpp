eee64739606561033c2d176706a8899ba21fef2e
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-12-17 18:04:55 +0300

    Update STL Silme Algoritmaları.cpp

--- Standard Template Library/Algorithms/STL Silme Algoritmaları.cpp
+++ Standard Template Library/Algorithms/STL Silme Algoritmaları.cpp
@@ -43,7 +43,33 @@ erase_if() kullanarak da bu işi yapabilirdik:
 auto n = erase_if(svec,[len](const string& s){return s.size()==len;});
 cout<< n <<"  eleman silindi";
 
+unique():
+Ex:
+int main(){
+  using namespace std;
+  vector vec{2,5,6,5,5,7,8,3,3,3,3,9,9,8,2,9,1};
+  vec.erase(unique(vec.begin(),vec.end()),vec.end()); //Her sayıdan sadece birer adet kaldı. Yine erase-remove'daki gibi erase-unique kullanıldı.
 
+}
+Ex:
+int main(){
+  using namespace std;
+  vector vec{2,-5,6,5,5,7,8,-3,3,3,3,-9,9,8,2,9,1};
+  vec.erase(unique(vec.begin(),vec.end(),[](int a, int b){return abs(a)==abs(b);}),vec.end()); // negatif değerlerden de sonradan gelenler silindi.
+//2,-5,6,7,8,-3,-9,1
+}
+
+Elle girilen dizide boşlukların sayısını bire indirme örneği:
+Ex:
+int main(){
+  using namespace std;
+  string str;
+  cout<<"bir yazi girin: ";
+  getline(cin,str);
+  cout<<"["<<str<<"]\n";
+  str.erase(unique(str.begin(),str.end(),[](char c1,char c2){return isspace(c1)&&isspace(c2);}),str.end());
+  cout<<str;
+}
 
 
 
