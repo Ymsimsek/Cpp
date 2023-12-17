36d07c904ae2df184489ee74d802930b31cd0348
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-12-17 17:44:26 +0300

    Create STL Silme Algoritmaları.cpp

---
+++ Standard Template Library/Algorithms/STL Silme Algoritmaları.cpp
@@ -0,0 +1,73 @@
+/*
+-> STL'de remove, remove_if, unique olarak adlandırılan, silme işlevini yerine getiren algoritmalardır. Silme algoritmalarının geri
+dönüş değeri türü iter'dir. Logic_end dediğimiz bir değer döndürür.
+
+remove: Bir rangede belirli değere sahip olan öğeleri siler. Aslında fiilen bir silme işlemi yapmaz. Range'i öğeler silinmiş gibi düzenler 
+ve silinmemiş son öğeden sonraki konumu döndürür. Remove'un döndürdüğü iterator'u vektorün end() ile range yapıp erase'e gönderirsek
+gerçekten silinmiş olur.
+
+remove_if: Belirli koşulları sağlayan, unary predicate'ın true değer verdiği öğeleri siler.
+
+unique: Ardışık aynı değerdeki öğelerin sayısını bire indirir.
+
+remove():
+Ex:
+int main(){
+  using namespace std;
+  vector<string>svec{"ali","can","veli","mehmet","can"};
+  auto logic_end_iter = remove(svec.begin(),svec.end(),"can");
+  cout<<"size= "<<svec.size()<<'\n'; //size: 5
+
+  cout<<"silinmemis oge sayisi: "<<distance(svec.begin(),logic_end_iter)<<'\n';
+  cout<<"silinmis oge sayisi: "<<distance(logic_end_iter,svec.end())<<'\n';
+
+  svec.erase(logic_end_iter,svec.end());
+  cout<<"size= "<<svec.size()<<'\n'; //size: 3
+}
+
+Remove-Erase Idiom: Tek satırda yukarıdaki işlev tamamlanabilir. 
+svec.erase(remove(svec.begin(),svec.end(),"can"),svec.end());
+
+C++20 ile doğrudan containerdeki belirli öğelerin tümünü silip, silinen öğe sayısını döndüren bir erase() overload'ı dile eklendi.
+auto n = erase(svec,"can"); //C++20
+
+remove_if(): Removeden farklı olarak bir unary predicate argümanı alır.
+Ex:
+int main(){
+  vector<string>svec{"ali","can","veli","mehmet","can"};
+  size_t len = 6;
+  svec.erase(remove_if(svec.begin(),svec.end(),[len](const string& s){return s.size()==len;}),svec.end());//mehmet silindi uzunluğu 6 olanları silmek
+}
+
+erase_if() kullanarak da bu işi yapabilirdik:
+auto n = erase_if(svec,[len](const string& s){return s.size()==len;});
+cout<< n <<"  eleman silindi";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/ 
