7165b4f3f91fd0dfb758c1cf0deb5ac4b25674d6
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-12-18 00:58:18 +0300

    Create Sorted Range Algorithms.cpp

---
+++ Standard Template Library/Algorithms/Sorted Range Algorithms.cpp
@@ -0,0 +1,36 @@
+/*
+-> Lower Bound: bir anahtar için elde edilen bir konum bilgisidir.  Verilen bir değer için öyle bir konumdur ki o değeri sırayı bozmadan 
+ekleyebileceğimiz ilk konumdur. Yani anahtardan büyük eşit olan ilk öğenin konumudur. 
+
+-> Upper Bound: bir anahtar için elde edilen bir konum bilgisidir.  Verilen bir değer için öyle bir konumdur ki o değeri sırayı bozmadan 
+ekleyebileceğimiz son konumdur. Yani anahtardan büyük olan ilk öğenin konumudur. 
+
+ -> Lower Bound- Upper Bound arasındaki öğe sayısı bize key'den kaç tane olduğunu gösterir ve bu range'e equal range denir. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
