633dc2ed015fe9086beb422df024565784ef1451
Yıldırım Mehmet Şimşek <145456982+Ymsimsek@users.noreply.github.com>
2023-12-18 12:58:26 +0300

    Update Sorted Range Algorithms.cpp

--- Standard Template Library/Algorithms/Sorted Range Algorithms.cpp
+++ Standard Template Library/Algorithms/Sorted Range Algorithms.cpp
@@ -1,11 +1,18 @@
 /*
--> Lower Bound: bir anahtar için elde edilen bir konum bilgisidir.  Verilen bir değer için öyle bir konumdur ki o değeri sırayı bozmadan 
+-> lower_bound: bir anahtar için elde edilen bir konum bilgisidir.  Verilen bir değer için öyle bir konumdur ki o değeri sırayı bozmadan 
 ekleyebileceğimiz ilk konumdur. Yani anahtardan büyük eşit olan ilk öğenin konumudur. 
 
--> Upper Bound: bir anahtar için elde edilen bir konum bilgisidir.  Verilen bir değer için öyle bir konumdur ki o değeri sırayı bozmadan 
+-> upper_bound: bir anahtar için elde edilen bir konum bilgisidir.  Verilen bir değer için öyle bir konumdur ki o değeri sırayı bozmadan 
 ekleyebileceğimiz son konumdur. Yani anahtardan büyük olan ilk öğenin konumudur. 
 
- -> Lower Bound- Upper Bound arasındaki öğe sayısı bize key'den kaç tane olduğunu gösterir ve bu range'e equal range denir. 
+-> Lower Bound- Upper Bound arasındaki öğe sayısı bize key'den kaç tane olduğunu gösterir ve bu range'e equal_range denir. 
+
+-> set_intersection: İki sıralı range'in kesişimlerini buldurur. 
+
+-> set_union: İki sıralı range'in birleşimleri bulunur.
+
+-> set_difference: Birinci range'de olup ikincide olmayan elemanlar bulunur. 
+
 
 
 
